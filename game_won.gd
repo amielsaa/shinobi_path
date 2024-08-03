77ee8bf7e367f46b0cf1d25d205cdfd68285f853
yoavital <yoavital98@gmail.com>
2024-08-03 22:14:59 +0300

    up

--- game_won.gd
+++ game_won.gd
@@ -1,24 +1,19 @@
 extends Control
 
-func turn_visible():
-	$GameOver.visible = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _on_button_pressed():
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://survivors_game.tscn")
 
-
-func _on_button_2_pressed():
+func _on_instruction_button_pressed():
 	get_tree().change_scene_to_file("res://MainMenu.tscn")
 
+func _on_quit_button_pressed():
+	get_tree().quit()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://survivors_game.tscn")
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file("res://MainMenu.tscn")
-
 
